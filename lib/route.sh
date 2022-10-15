e666b8a52827860a5311085411fb939452d97a6b
AzazelSensei <89297042+AzazelSensei@users.noreply.github.com>
2022-10-16 00:18:30 +0300

    For update

--- lib/route.sh
+++ lib/route.sh
@@ -2,4 +2,8 @@ set -e
 flutter clean
 flutter pub get
 flutter pub run build_runner watch --delete-conflicting-outputs
+flutter packages pub run build_runner watch  
 flutter pub run build_runner build --enable-experiment=enhanced-enums --delete-conflicting-outputs
+
+
+flutter gen-l10n
